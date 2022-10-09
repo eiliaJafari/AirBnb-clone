91b53e9848fe71161de24651fde0cc180787a24e
ilyaJafari99 <85680466+ilyaJafari99@users.noreply.github.com>
2022-10-09 23:32:45 +0330

    Third Commit
    Created users application, Altered Django admin panel

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    """
+    Custom User Model
+    """
+
+    GENDER_MALE = "Male"
+    GENDER_FEMALE = "Female"
+    GENDER_OTHER = "Other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "English"
+    LANGUAGE_Farsi = "Farsi"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_Farsi, "Farsi"),
+    )
+
+    # CURRENCY_IRR = "IRR"
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, blank=True)
+    superhost = models.BooleanField(default=False)
