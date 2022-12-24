9cb583c383a65d3ea2624ddcc77ab584cd84cc1e
ilyaJafari99 <85680466+ilyaJafari99@users.noreply.github.com>
2022-12-24 20:01:48 +0330

    Eighteenth commit
    Finished designing the room cards and i am very happy about it !

--- rooms/models.py
+++ rooms/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django_countries.fields import CountryField
 from django.urls import reverse
+from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
-    """Abstract Items"""
+
+    """Abstract Item"""
 
     name = models.CharField(max_length=80)
 
@@ -18,26 +18,42 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class RoomType(AbstractItem):
+
+    """RoomType Model Definition"""
+
     class Meta:
         verbose_name = "Room Type"
 
 
 class Amenity(AbstractItem):
+
+    """Amenity Model Definition"""
+
     class Meta:
         verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
+
+    """Facility Model Definition"""
+
+    pass
+
     class Meta:
         verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
+
+    """HouseRule Model Definition"""
+
     class Meta:
         verbose_name = "House Rule"
 
 
 class Photo(core_models.TimeStampedModel):
+
+    """Photo Model Definition"""
 
     caption = models.CharField(max_length=80)
     file = models.ImageField(upload_to="room_photos")
@@ -49,12 +65,9 @@ class Photo(core_models.TimeStampedModel):
 
 class Room(core_models.TimeStampedModel):
 
-    """Room Model Defenition"""
+    """Room Model Definition"""
 
     name = models.CharField(max_length=140)
-    host = models.ForeignKey(
-        user_models.User, related_name="rooms", on_delete=models.CASCADE
-    )
     description = models.TextField()
     country = CountryField()
     city = models.CharField(max_length=80)
@@ -67,12 +80,15 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    room_type = models.ForeignKey(
-        RoomType, related_name="room_types", on_delete=models.SET_NULL, null=True
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
     )
-    amenities = models.ManyToManyField(Amenity, related_name="rooms", blank=True)
-    facilities = models.ManyToManyField(Facility, related_name="rooms", blank=True)
-    house_rules = models.ManyToManyField(HouseRule, related_name="rooms", blank=True)
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
@@ -87,8 +103,19 @@ class Room(core_models.TimeStampedModel):
     def total_rating(self):
         all_reviews = self.reviews.all()  # type: ignore
         all_ratings = 0
-
-        if len(all_reviews) > 0:  # type: ignore
+        if len(all_reviews) > 0:
             for review in all_reviews:
                 all_ratings += review.rating_average()
-            return "{:.2f}".format(all_ratings / len(all_reviews))
+            return round(all_ratings / len(all_reviews), 2)
+        return 0
+
+    def first_photo(self):
+        try:
+            (photo,) = self.photos.all()[:1]  # type: ignore
+            return photo.file.url
+        except ValueError:
+            return None
+
+    def get_next_four_photos(self):
+        photos = self.photos.all()[1:5]  # type: ignore
+        return photos
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+      borderRadius: {
+        xl: "1.5rem"
+      },
+      minHeight: {
+        "50vh": "50vh",
+        "75vh": "75vh"
+      }
+    }
   },
-  plugins: [],
-}
+  variants: {},
+  plugins: []
+};
