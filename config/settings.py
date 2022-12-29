94e092b915fee8e60300c0f6bc85522a0f38c9b4
ilyaJafari99 <85680466+ilyaJafari99@users.noreply.github.com>
2022-12-29 12:44:08 +0330

    Twenty-sixth commit
    Translated the website to Farsi with the help of a JS script and Django translation.

--- config/settings.py
+++ config/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -153,3 +154,6 @@ MEDIA_URL = "/media/"
 
 # Auth
 LOGIN_URL = "users/login"
+
+# Locale
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
--- users/models.py
+++ users/models.py
@@ -1,16 +1,7 @@
-# import uuid
 from django.utils.translation import gettext_lazy as _
-
-# from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# from django.core.mail import send_mail
-# from django.utils.html import strip_tags
 from django.urls import reverse
-
-# from django.shortcuts import reverse
-# from django.template.loader import render_to_string
 
 
 class User(AbstractUser):
@@ -24,17 +15,17 @@ class User(AbstractUser):
     GENDER_OTHER = "Other"
 
     GENDER_CHOICES = (
-        (GENDER_MALE, "Male"),
-        (GENDER_FEMALE, "Female"),
-        (GENDER_OTHER, "Other"),
+        (GENDER_MALE, _("Male")),
+        (GENDER_FEMALE, _("Female")),
+        (GENDER_OTHER, _("Other")),
     )
 
-    LANGUAGE_ENGLISH = "English"
-    LANGUAGE_Farsi = "Farsi"
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_Farsi = "fa"
 
     LANGUAGE_CHOICES = (
-        (LANGUAGE_ENGLISH, "English"),
-        (LANGUAGE_Farsi, "Farsi"),
+        (LANGUAGE_ENGLISH, _("English")),
+        (LANGUAGE_Farsi, _("Farsi")),
     )
 
     LOGIN_EMAIL = "email"
@@ -47,13 +38,20 @@ class User(AbstractUser):
 
     first_name = models.CharField(_("first name"), max_length=30, blank=True)
 
-    avatar = models.ImageField(blank=True, upload_to="avatars")
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
-    bio = models.TextField(default="", blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    gender = models.CharField(
+        _("gender"), choices=GENDER_CHOICES, max_length=10, blank=True
+    )
+    bio = models.TextField(_("bio"), blank=True)
     birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=10, blank=True, default=LANGUAGE_Farsi
+        _("language"),
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        blank=True,
+        default=LANGUAGE_Farsi,
     )
+
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=120, default="", blank=True)
@@ -63,21 +61,3 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("users:profile", kwargs={"pk": self.pk})
-
-    # def verify_email(self):
-    #     if self.email_verified is False:
-    #         secret = uuid.uuid4().hex[:20]
-    #         self.email_secret = secret
-    #         html_message = render_to_string(
-    #             "emails/verify_email.html", {"secret": secret}
-    #         )
-    #         send_mail(
-    #             _("Verify Account"),
-    #             strip_tags(html_message),
-    #             settings.EMAIL_FROM,
-    #             [self.email],
-    #             fail_silently=False,
-    #             html_message=html_message,
-    #         )
-    #         self.save()
-    #     return
