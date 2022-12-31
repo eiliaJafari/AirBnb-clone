35e2d6ae0a2de988ea84c07d1b6170b43139ce74
ilyaJafari99 <85680466+ilyaJafari99@users.noreply.github.com>
2022-12-31 12:56:32 +0330

    Thirtieth commit
    Fixed the search for rooms functionality

--- rooms/forms.py
+++ rooms/forms.py
@@ -6,26 +6,26 @@ from . import models
 class SearchForm(forms.Form):
     city = forms.CharField(initial="Anywhere")
     country = CountryField(default="IR").formfield()
-    room_type = forms.ModelChoiceField(
-        required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
-    )
-    price = forms.IntegerField(required=False)
-    guests = forms.IntegerField(required=False)
-    bedrooms = forms.IntegerField(required=False)
-    beds = forms.IntegerField(required=False)
-    baths = forms.IntegerField(required=False)
-    instant_book = forms.BooleanField(required=False)
-    superhost = forms.BooleanField(required=False)
-    amenities = forms.ModelMultipleChoiceField(
-        required=False,
-        queryset=models.Amenity.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-    )
-    facilities = forms.ModelMultipleChoiceField(
-        required=False,
-        queryset=models.Facility.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-    )
+    # room_type = forms.ModelChoiceField(
+    #     required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
+    # )
+    # price = forms.IntegerField(required=False)
+    # guests = forms.IntegerField(required=False)
+    # bedrooms = forms.IntegerField(required=False)
+    # beds = forms.IntegerField(required=False)
+    # baths = forms.IntegerField(required=False)
+    # instant_book = forms.BooleanField(required=False)
+    # superhost = forms.BooleanField(required=False)
+    # amenities = forms.ModelMultipleChoiceField(
+    #     required=False,
+    #     queryset=models.Amenity.objects.all(),
+    #     widget=forms.CheckboxSelectMultiple,
+    # )
+    # facilities = forms.ModelMultipleChoiceField(
+    #     required=False,
+    #     queryset=models.Facility.objects.all(),
+    #     widget=forms.CheckboxSelectMultiple,
+    # )
 
 
 class CreatePhotoForm(forms.ModelForm):
--- rooms/views.py
+++ rooms/views.py
@@ -35,16 +35,6 @@ class SearchView(View):
 
                 city = form.cleaned_data.get("city")
                 country = form.cleaned_data.get("country")
-                room_type = form.cleaned_data.get("room_type")
-                price = form.cleaned_data.get("price")
-                guests = form.cleaned_data.get("guests")
-                bedrooms = form.cleaned_data.get("bedrooms")
-                beds = form.cleaned_data.get("beds")
-                baths = form.cleaned_data.get("baths")
-                instant_book = form.cleaned_data.get("instant_book")
-                superhost = form.cleaned_data.get("superhost")
-                amenities = form.cleaned_data.get("amenities")
-                facilities = form.cleaned_data.get("facilities")
 
                 filter_args = {}
 
@@ -52,36 +42,6 @@ class SearchView(View):
                     filter_args["city__startswith"] = city
 
                 filter_args["country"] = country
-
-                if room_type is not None:
-                    filter_args["room_type"] = room_type
-
-                if price is not None:
-                    filter_args["price__lte"] = price
-
-                if guests is not None:
-                    filter_args["guests__gte"] = guests
-
-                if bedrooms is not None:
-                    filter_args["bedrooms__gte"] = bedrooms
-
-                if beds is not None:
-                    filter_args["beds__gte"] = beds
-
-                if baths is not None:
-                    filter_args["baths__gte"] = baths
-
-                if instant_book is True:
-                    filter_args["instant_book"] = True
-
-                if superhost is True:
-                    filter_args["host__superhost"] = True
-
-                for amenity in amenities:
-                    filter_args["amenities"] = amenity
-
-                for facility in facilities:
-                    filter_args["facilities"] = facility
 
                 qs = models.Room.objects.filter(**filter_args).order_by("-created")
 
